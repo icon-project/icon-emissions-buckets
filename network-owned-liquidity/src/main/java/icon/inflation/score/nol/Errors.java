b9162bd649de4dc29a7d681de41941c545417cfe
AntonAndell <anton@andell.eu>
2024-02-16 08:26:11 +0100

    fix: Add check for zero value in tokenFallback

--- network-owned-liquidity/src/main/java/icon/inflation/score/nol/Errors.java
+++ network-owned-liquidity/src/main/java/icon/inflation/score/nol/Errors.java
@@ -3,6 +3,7 @@ package icon.inflation.score.nol;
 
 public class Errors {
     public static final String TOKEN_FALLBACK_DATA_EMPTY = "Token Fallback: Data can't be empty";
+    public static final String TOKEN_FALLBACK_ZERO_VALUE = "Token Fallback: Value can't be zero";
     public static final String IRC31_METHOD_NOT_FOUND = "IRC31: method not found";
     public static final String ORDER_LIMIT_REACHED = "Order is above configured limit";
     public static final String LP_OVER_SLIPPAGE_LIMIT = "The price of the liquidity pool is to far off the oracle price of supplied assets";
